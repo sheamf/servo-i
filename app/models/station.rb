9a02ee7b9d36bcbe9a88aa07e614fa8be17cead4
sheamf <miles_shea@yahoo.com>
2014-03-31 06:39:48 -0600

    fix station model

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,11 +1,19 @@
 class Station < ActiveRecord::Base
 
-	# has_one :location, class_name: 'StationLocation'
-  has_many :station_locations
-  # belongs_to :station_location
+	has_one :location, class_name: 'StationLocation'
 	has_many :station_contacts
 
 	belongs_to :owner
+
+
+  def latitude
+    self.location.latitude
+  end
+
+  def longitude
+    self.location.longitude
+  end
+
 
 
 end
